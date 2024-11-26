ec220382db0d1b49aadcb2557e28b11dd5d8e17b
Antonio Queb <antonioqueb@alphaqueb.online>
2024-11-26 14:40:05 -0600

    Update

--- __manifest__.py
+++ __manifest__.py
@@ -4,7 +4,7 @@
     'summary': 'Vincula órdenes de compra con diversas entidades',
     'author': 'ALPHAQUEB CONSULTING SAS',
     'category': 'Purchases',
-    'depends': ['purchase', 'fleet', 'project', 'maintenance', 'hr', 'calendar'],
+    'depends': ['purchase', 'fleet', 'project', 'maintenance', 'hr', 'calendar','custom_purchase_extension',],
     'data': [
         'security/ir.model.access.csv',
         'views/purchase_order_view_inherit.xml',
